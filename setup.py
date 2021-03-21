a8db62b719987d9b4d6b361ead199b9afcd42ae6
Simon <simon.g.robles@gmail.com>
2021-03-21 11:16:58 -0700

    fixed import error for entrypoint

--- setup.py
+++ setup.py
@@ -1,3 +1,29 @@
-import setuptools
+from setuptools import setup
 
-setuptools.setup()
+setup(
+    name="timerpal",
+    version="0.0.1",
+    description="Simple terminal timer with curses",
+    author="Simon Robles",
+    author_email="simon.g.robles@gmail.com",
+    license="GPLv3",
+    url="https://github.com/Simon-CPSC/TimerPal",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Topic :: Utilities"
+    ],
+    install_requires=[
+        "click >= 2.0",
+        "pyfiglet >= 0.7",
+        "python-dateutil"
+    ],
+    py_modules=["timerpal"],
+    entry_points={
+        'console_scripts': [
+                "timerpal=timerpal:main"
+            ],
+        },
+)
